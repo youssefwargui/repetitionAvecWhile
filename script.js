b48284cd0072c756dd04accb344c6b3e85e8117f
youssefwargui <youssefwargui100@gmail.com>
2026-01-20 07:09:27 +0100

    ex

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+// 1 — Somme de N
 
+function sommeDeN(n) {
+    
+    
+    var somme = 0
+    while ( n > somme) {
+    
+
+
+    }
+return somme 
+}
+
+// 2 — Factorielle de N
+
+
+// 3 — Répéter une chaîne
+
+function répéterChaîne(chaîne, nombre) {
+   
+}
